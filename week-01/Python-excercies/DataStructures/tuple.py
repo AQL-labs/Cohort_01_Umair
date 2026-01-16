754ab1edd37a65d4f3b08b200a3abd6753688bce
Umair Ashraf <36963276+Umair777@users.noreply.github.com>
2026-01-17 04:31:37 +0530

    renames and DS beginning

---
+++ week-01/Python-excercies/DataStructures/tuple.py
@@ -0,0 +1,12 @@
+# Create your first tuple
+
+tuple1 = ("disco",10,1.2 )
+print("First element:", tuple1[0])
+print("Last element:", tuple1[-1])
+
+# sorting a tuple
+
+tuple2 = (2,5,3,1,4)
+print("Original Tuple:", tuple2)
+sorted_tuple = sorted(tuple2)
+print("Sorted Tuple:", sorted_tuple)
